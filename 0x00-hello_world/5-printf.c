1a9eedf5845d62ca21f728c7e9dc230c8e9555e3
Mosa Matlana <mosa97matlana@gmail.com>
2022-09-08 17:02:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing messsage to console
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
