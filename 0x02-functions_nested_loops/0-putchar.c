35dcc4487aa929881f0918d4d66154b8956a44d5
Mosa Matlana <mosa97matlana@gmail.com>
2022-09-14 16:10:58 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet -Entry point to program
+  * Description: Prints alphabet
+  * Return: Nothing -Null
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
