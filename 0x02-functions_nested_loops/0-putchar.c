24a896cff5119ce5f3539aa403c355d8901cc1fb
Mosa Matlana <mosa97matlana@gmail.com>
2022-09-14 16:31:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
 #include "main.h"
-/**
-  * print_alphabet -Entry point to program
-  * Description: Prints alphabet
-  * Return: Nothing -Null
- */
-void print_alphabet(void)
-{
-	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+/**
+ * main -Entry point
+ * Description: Prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char str[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(ch);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
+	return (0);
 }
