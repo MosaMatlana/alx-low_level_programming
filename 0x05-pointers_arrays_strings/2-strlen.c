2742251d2dfe7211a2f8fc90eb6db9de86267730
Mosa Matlana <mosa97matlana@gmail.com>
2022-09-20 11:16:57 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen -Finds length of input string, similar to strlen
+ * @s: Input string
+ * Return: Length of string, int
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	/* s is the location of the input string in memory => s =&string[0] */
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
