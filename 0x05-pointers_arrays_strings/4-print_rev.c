76d19389dc619ae693f19c787baf4d7c0b1e8e2b
Mosa Matlana <mosa97matlana@gmail.com>
2022-09-20 13:32:54 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev -Prints a string to stdout in reverse
+ * @s: Input string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	length -= 1;
+
+	while (length != -1)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}
