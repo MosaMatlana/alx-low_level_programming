2b3eacb8ad0155adbb2fb26bbace6290c02cd0df
Mosa Matlana <mosa97matlana@gmail.com>
2022-09-21 16:49:55 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - Copies src to dest
+ * @dest: New string
+ * @src: String to be copied
+ * @n: Number of bytes of src to be concatinated with dest
+ *
+ * Return: New dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (j < n)
+	{
+		if (src[j] != '\0')
+			dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+		dest[j] = '\0';
+	j++;	
+	return (dest);
+}
