88aadea4dd57f9de3d29f93c7432d74e11db634b
Mosa Matlana <mosa97matlana@gmail.com>
2022-09-21 16:47:08 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat - Concatinates two strings
+ * @dest: Original string
+ * @src: String to be appended to dest
+ * @n: Number of bytes of src to be concatinated with dest
+ *
+ * Return: New dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (j < n)
+	{
+		if (src[j] != '\0')
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
+		else
+			return (dest);
+	}
+	return (dest);
+}
